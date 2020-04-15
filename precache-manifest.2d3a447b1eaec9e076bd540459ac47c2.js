5fe99eb0e231300756ecb674537067a94eb11cfa
Alejandro Pareja Londoño <alejopar2012@gmail.com>
2020-04-15 14:07:23 -0500

    Updates

---
+++ precache-manifest.2d3a447b1eaec9e076bd540459ac47c2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c05648521ae01b1104bbb362155bf0",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "64466f2e452a2f81e51d",
+    "url": "/developerFolio/static/css/main.a571b26c.chunk.css"
+  },
+  {
+    "revision": "15681790f6b53aece760",
+    "url": "/developerFolio/static/js/2.5ca2732f.chunk.js"
+  },
+  {
+    "revision": "64466f2e452a2f81e51d",
+    "url": "/developerFolio/static/js/main.5c549659.chunk.js"
+  },
+  {
+    "revision": "96b21a6ac5552cfe9203",
+    "url": "/developerFolio/static/js/runtime-main.99ea03dd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/developerFolio/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "9aa3135acea46c7d7964e89ee8dd1347",
+    "url": "/developerFolio/static/media/choice.9aa3135a.svg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/developerFolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/developerFolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/developerFolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/developerFolio/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/developerFolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/developerFolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/developerFolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/developerFolio/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/developerFolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/developerFolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/developerFolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/developerFolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/developerFolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/developerFolio/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/developerFolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/developerFolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69b019b6403f09be649e2772005025a6",
+    "url": "/developerFolio/static/media/manOnTable.69b019b6.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/developerFolio/static/media/programming.c1a95f73.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/developerFolio/static/media/talksCardBack.de3da806.svg"
+  }
+]);
